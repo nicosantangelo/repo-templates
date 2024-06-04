b3d18c440fc64d1c00ae2587fbc0e17bce814dc7
Nicolas Santangelo <santangelonicolas@gmail.com>
2024-06-04 17:38:17 -0300

    implement tailwind

--- website-template/src/App.tsx
+++ website-template/src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,22 +9,40 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img
+            className="h-36 inline p-6 transition-[filter] duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]"
+            src={viteLogo}
+            alt="Vite logo"
+          />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img
+            src={reactLogo}
+            className="h-36 inline p-6 transition-[filter] duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa] animate-[spin_infinite_20s_linear]"
+            alt="React logo"
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className="text-[3.2rem] leading-[1.1] font-bold my-[2.15rem]">
+        Vite + React
+      </h1>
+      <div className="p-8">
+        <button
+          className="rounded-lg text-base leading-tight font-medium font-[inherit] border border-solid border-transparent py-[0.6rem] px-[1.2rem] bg-[#1a1a1a] cursor-pointer
+          transition-[border-color] 
+          hover:border-[#646cff]
+          focus:outline-4 focus:outline-style-[auto] focus:outline-[-webkit-focus-ring-color]
+          focus-visible:outline-4 focus-visible:outline-style-[auto] focus-visible:outline-[-webkit-focus-ring-color]"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        <p className="my-4">
+          Edit <code className="text-[0.813rem] font-mono">src/App.tsx</code>{" "}
+          and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="my-4 text-[#888]">
         Click on the Vite and React logos to learn more
       </p>
     </>
